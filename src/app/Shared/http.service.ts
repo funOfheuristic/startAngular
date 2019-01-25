2c3e0f9b0f26eec8654b870beeec201861f255e7
Subrat <subrat@Subrats-MacBook.local>
2019-01-25 08:55:36 +0530

    Email

--- src/app/Shared/http.service.ts
+++ src/app/Shared/http.service.ts
@@ -1,19 +1,22 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class HttpService {
-
   test = "How r u?";
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
-  httpGet(url){
+  httpGet(url) {
     return this.http.get(url);
   }
 
-  httpPost(url, {}){
-    return this.http.post(url, {"name": "Subrat"});
+  httpPost(url, {}) {
+    return this.http.post(url, { name: "Subrat" });
+  }
+
+  sendEmail(url, data) {
+    return this.http.post(url, data);
   }
 }
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,51 +1,75 @@
-import { Component, OnInit, OnChanges } from '@angular/core';
-import { HttpService } from '../Shared/http.service';
-import { FormControl, Validators } from '@angular/forms';
-import { DogComponent } from '../dummy/dog/dog.component';
-import { CowComponent } from '../dummy/cow/cow.component';
-import { CatComponent } from '../dummy/cat/cat.component';
+import { Component, OnInit, OnChanges } from "@angular/core";
+import { HttpService } from "../Shared/http.service";
+import { FormControl, Validators } from "@angular/forms";
+import { DogComponent } from "../dummy/dog/dog.component";
+import { CowComponent } from "../dummy/cow/cow.component";
+import { CatComponent } from "../dummy/cat/cat.component";
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  selector: "app-login",
+  templateUrl: "./login.component.html",
+  styleUrls: ["./login.component.css"]
 })
 export class LoginComponent implements OnInit {
-  image= "https://images.freeimages.com/images/large-previews/7bc/bald-eagle-1-1400106.jpg";
+  image =
+    "https://images.freeimages.com/images/large-previews/7bc/bald-eagle-1-1400106.jpg";
   name1;
   age;
+  loading = false;
+  buttionText = "Submit";
 
-  emailFormControl = new FormControl('', [
+  emailFormControl = new FormControl("", [
     Validators.required,
-    Validators.email,
+    Validators.email
   ]);
 
-  nameFormControl = new FormControl('', [
+  nameFormControl = new FormControl("", [
     Validators.required,
     Validators.minLength(4)
   ]);
 
   dummyComponent = DogComponent;
 
-  constructor(public http: HttpService) { }
-  
+  constructor(public http: HttpService) {}
 
   ngOnInit() {
     console.log(this.http.test);
   }
 
-  assignComponent(component){
-    if(component === 'cow')
-      this.dummyComponent = CowComponent;
-    else if(component === 'dog')
-      this.dummyComponent = DogComponent;
-    else
-      this.dummyComponent = CatComponent;
+  assignComponent(component) {
+    if (component === "cow") this.dummyComponent = CowComponent;
+    else if (component === "dog") this.dummyComponent = DogComponent;
+    else this.dummyComponent = CatComponent;
   }
 
-  changeImage(){
+  changeImage() {
     this.http.test = "changed";
-    this.image = "https://images.pexels.com/photos/635529/pexels-photo-635529.jpeg?auto=compress&cs=tinysrgb&h=650&w=940"
+    this.image =
+      "https://images.pexels.com/photos/635529/pexels-photo-635529.jpeg?auto=compress&cs=tinysrgb&h=650&w=940";
   }
 
+  register() {
+    this.loading = true;
+    this.buttionText = "Submiting...";
+    let user = {
+      name: this.nameFormControl.value,
+      email: this.emailFormControl.value
+    }
+    this.http.sendEmail("http://localhost:3000/sendmail", user).subscribe(
+      data => {
+        let res:any = data; 
+        console.log(
+          `👏 > 👏 > 👏 > 👏 ${user.name} is successfully register and mail has been sent and the message id is ${res.messageId}`
+        );
+      },
+      err => {
+        console.log(err);
+        this.loading = false;
+        this.buttionText = "Submit";
+      },() => {
+        this.loading = false;
+        this.buttionText = "Submit";
+      }
+    );
+  }
 }
